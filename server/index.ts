8502b1fb1d6696ea06b6eab57ade60198ce2ffa8
jhonrs <jhonrs812@gmail.com>
2021-11-08 18:07:18 -0500

    Technical test solution

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,63 @@
 import express from 'express';
+import { json } from 'stream/consumers';
+var cors = require('cors');
 
 const app = express();
 const port = 3333;
 
-app.get( "/api", ( req, res ) => {
-  return res.json({ version: 0.1 });
+
+const bp = require('body-parser');
+app.use(cors());
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+let base = [
+  { name: "Jhon Reyes" },
+  { name: "Julieth Salazar" },
+  { name: "Juan Bravo" },
+  { name: "Luis Diaz" },
+  { name: "Juanes Perez" },
+  { name: "Mateo Valencia" },
+  { name: "Saul Luas" },
+  { name: "Nicole Melo" },
+  { name: "Aurora Sanchez" },
+  { name: "Saul Valencia" },
+  { name: "Mateo Luas" },
+  { name: "Steph Melo" },
+  { name: "Monica Ramirez" }
+];
+
+var fs = require('fs');
+app.get("/api", (req, res) => {
+
+  const nums = Math.floor(Math.random() * 4) + 5;
+  const shuffleArray = base.sort(() => 0.5 - Math.random());
+  return res.json({ names: shuffleArray.slice(0, nums) });
 });
 
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.put("/counter", (req, res) => {
+  const { name } = req.body;
+  fs.readFile('server/counter.txt', 'utf8', (err: string, data: string) => {
+    if (err) console.error(err);
+    const currentNames = data.split("\n");
+    const clickedName = currentNames.findIndex((item: string) => item.includes(name));
+    if (clickedName > -1) {
+      currentNames[clickedName] = name + "," + (parseInt(currentNames[clickedName].split(",")[1]) + 1);
+    } else {
+      currentNames.push(name + ",1");
+    }
+    fs.writeFile("server/counter.txt", currentNames.join("\n"), function (err: string) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("El archivo fue creado correctamente");
+    });
+    console.log(currentNames);
+
+  });
+  return res.json(req.body);
+})
+
+app.listen(port, () => {
+  console.log(`Hello Jhon the server started at http://localhost:${port}`);
+});
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,47 @@
-import { Container, CssBaseline, Paper, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Container, CssBaseline, List, ListItem, Paper, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
 
 function App() {
+
+  const [names, setNames] = useState([]);
+
+  useEffect(() => {
+
+    const loadData = async () => {
+      try {
+        const res = await fetch('http://localhost:3333/api');
+        const data = await res.json();
+        setNames(data.names);
+      } catch (error) {
+        return console.error(error);
+
+      }
+    }
+
+    const dataInterval = setInterval(loadData, 5000);
+    loadData();
+    return () => {
+      clearInterval(dataInterval);
+    }
+  }, []);
+
+  const handleNameClick = async (name: string) => {
+    try {
+      await fetch('http://localhost:3333/counter', {
+        method: "PUT",
+        body: JSON.stringify({ name }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+
+      });
+    } catch (error) {
+      return console.error(error);
+
+    }
+  }
+
   return (<>
     <CssBaseline />
     <Box bgcolor="#eeeeee" minHeight="100vh" p={4}>
@@ -10,6 +49,11 @@ function App() {
         <Paper>
           <Box p={4}>
             <Typography variant="h4">People to click</Typography>
+            <List>
+              {names.map(({ name }) =>
+                <ListItem button onClick={() => handleNameClick(name)} key={name}>{name}</ListItem>
+              )}
+            </List>
           </Box>
         </Paper>
       </Container>
